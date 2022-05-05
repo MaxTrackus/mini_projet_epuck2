8e5efe845ae7faa8d2e8b2dee3c5c1f597e89093
jsuchet <jysuchet@gmail.com>
2022-05-05 11:48:10 +0200

    Proxi clean and created function to get activation status or values for the proxi to other files

--- proxi.h
+++ proxi.h
@@ -1,11 +1,19 @@
 #ifndef PROXI_H
 #define PROXI_H
 
-//float get_distance_cm(void);
-//uint16_t get_line_position(void);
-//void process_image_start(void);
-void set_led_with_int(unsigned int led_int_number);
-void clear_led_with_int(unsigned int led_int_number);
+// Id proximity sensor
+#define PROX_FRONT_RIGHT_17 			0 // IR 1
+#define PROX_FRONT_LEFT_17				7 // IR 8
+#define PROX_FRONT_RIGHT_49 			1 // IR 2
+#define PROX_FRONT_LEFT_49				6 // IR 7
+#define PROX_RIGHT						2 // IR 3
+#define PROX_BACK_RIGHT 				3 // IR 4
+#define PROX_LEFT						5 // IR 6
+#define PROX_BACK_LEFT					4 // IR 5
+
+bool* get_prox_activation_status(int prox_detection_threshold);
+int* get_prox_value(void);
+
 void test_prox_with_leds(unsigned int sensor_number);
 void read_IR_start(void);
 void obstacles_avoidance_algorithm(void);
