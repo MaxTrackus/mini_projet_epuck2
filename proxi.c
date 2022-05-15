795c34daff3fd5ea372e8da6848282cb50d7a63b
MaxTrackus <m_theurillat@bluewin.ch>
2022-05-15 20:29:42 +0200

    NVM it seems to work

--- proxi.c
+++ proxi.c
@@ -1,7 +1,5 @@
-#include "ch.h" //usage ?
-#include "hal.h" //usage ?
-// #include <chprintf.h> //usage ?
-// #include <usbcfg.h> //usage ?
+#include "ch.h"
+#include "hal.h"
 #include <sensors/proximity.h>
 
 #include <proxi.h>
